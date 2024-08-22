896a715d34efa5a82778f8de5ba59a6ac49d91f2
SunLi2 <101978452+SunLi2@users.noreply.github.com>
2024-08-22 16:06:11 +0800

    Update PicUrlMapper.java

--- JLT-WXAPP/src/main/java/com/zsc/wxapp/mapper/PicUrlMapper.java
+++ JLT-WXAPP/src/main/java/com/zsc/wxapp/mapper/PicUrlMapper.java
@@ -17,23 +17,15 @@ public interface PicUrlMapper {
     // 新增方法，根据用户ID查找所有图片URL
     List<PicUrl> findAllPicUrlByUerid(@Param("pic_userid") String userid);
 
-    @Insert({
-            "<script>",
-            "INSERT INTO Pic_Url (jlt_pic_urlid, jlt_pic_uid, jlt_pic_url, ljt_pic_creattime) VALUES ",
-            "<foreach collection='picUrls' item='picUrl' separator=','>",
-            "(#{picUrl.jltPicUrlid}, #{picUrl.jltPicUid}, #{picUrl.jltPicUrl}, #{picUrl.ljtPicCreattime})",
-            "</foreach>",
-            "</script>"
-    })
     void insertPicUrls(List<PicUrl> picUrls);
 
-    @Select("SELECT jlt_pic_urlid FROM pic_url WHERE jlt_pic_urlid NOT IN (SELECT pt_id FROM price_tag1)")
+
     List<String> findUnusedPicUrls();
 
-    @Select("SELECT jlt_pic_url FROM pic_url WHERE jlt_pic_urlid = #{jltPicUrlid}")
+
     String findUrlById(String jltPicUrlid);
 
-    @Delete("DELETE FROM pic_url WHERE jlt_pic_urlid = #{jltPicUrlid}")
+
     void deleteById(String jltPicUrlid);
 
 }
