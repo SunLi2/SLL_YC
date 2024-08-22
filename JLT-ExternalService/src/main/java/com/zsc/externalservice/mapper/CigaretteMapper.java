a3b94a0a59adc6124ae4cfc064fcca61467119be
SunLi2 <101978452+SunLi2@users.noreply.github.com>
2024-08-22 16:01:11 +0800

    Update CigaretteMapper.java

--- JLT-ExternalService/src/main/java/com/zsc/externalservice/mapper/CigaretteMapper.java
+++ JLT-ExternalService/src/main/java/com/zsc/externalservice/mapper/CigaretteMapper.java
@@ -13,8 +13,5 @@ public interface CigaretteMapper {
      * 获取所有卷烟信息
      * @return
      */
-    @Select("select product_name, direct_retail_price, whole_sale_price, sale_begin_date, price_type_code_text, " +
-            "is_active, factory_simple_name, tar_qty, nicotine_qty, product_style_code_text, " +
-            "package_qty, package_qty2, product_code from ADS_SJZL_CC_TBC_PRODUCT;")
     List<AdsSjzlCcTbcProduct> getAll();
 }
